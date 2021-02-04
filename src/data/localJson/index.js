eecbbdd3254f74030eea6ada544ca26d52c6474f
vandiepbui <vandiepbui96@gmail.com>
2021-02-04 09:16:24 +0700

    update app

--- src/data/localJson/index.js
+++ src/data/localJson/index.js
@@ -1,33 +1,4 @@
-//Local Json data for development stage
-export const users = [
-  {
-    name: 'John',
-    age: '30',
-  },
-  {
-    name: 'John',
-    age: '30',
-  },
-  {
-    name: 'John',
-    age: '30',
-  },
-];
+//Local data JSON
 
-export const posts = [
-  {
-    id: 1,
-    title: 'First post',
-    content: 'Post content',
-  },
-  {
-    id: 2,
-    title: 'First post',
-    content: 'Post content',
-  },
-  {
-    id: 3,
-    title: 'First post',
-    content: 'Post content',
-  },
-];
+import onboarding from './onboarding';
+export {onboarding};
--- src/screens/Onboarding/index.js
+++ src/screens/Onboarding/index.js
@@ -1,45 +1,49 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Image} from 'components';
-
-import Onboarding from 'react-native-onboarding-swiper';
-
-const IMG_01 = require('assets/images/onboarding01.png');
-const IMG_02 = require('assets/images/onboarding02.png');
+import {View, Text, FlatList} from 'react-native';
+import {Image, Container} from 'components';
 
 import styles from './styles';
 
+import {onboarding as data} from 'data/localJson';
+
+import {dim} from 'utils/common';
+
+const WIDTH = dim.width;
+const HEIGHT = dim.height;
+
 const Index = (props) => {
+  const _renderItem = ({item, index}) => {
+    return (
+      <View style={[styles.pageWrapper, {width: WIDTH, height: HEIGHT}]}>
+        <View style={styles.imgWrapper}>
+          <Image
+            style={styles.image}
+            source={item.image}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.body}>
+          <Text style={styles.title}>{item.title}</Text>
+        </View>
+      </View>
+    );
+  };
   return (
-    <Onboarding
-      containerStyles={styles.container}
-      imageContainerStyles={styles.imageWrapper}
-      titleStyles={styles.titleStyles}
-      subTitleStyles={styles.subTitleStyles}
-      pages={[
-        {
-          backgroundColor: '#fff',
-          image: (
-            <Image source={IMG_01} style={styles.image} resizeMode="contain" />
-          ),
-          title: 'Cộng đồng thời trang lớn nhất Việt Nam',
-          subtitle:
-            'Cập nhật xu hướng, chia sẻ những bức ảnh thời trang đẹp nhất của bạn với cộng đồng của chúng tôi.',
-        },
-        {
-          backgroundColor: '#fff',
-          image: (
-            <Image source={IMG_02} style={styles.image} resizeMode="contain" />
-          ),
-          title: 'Onboarding',
-          subtitle: 'Done with React Native Onboarding Swiper',
-          containerStyles: styles.container,
-          imageContainerStyles: styles.imageWrapper,
-          titleStyles: styles.titleStyles,
-          subTitleStyles: styles.subTitleStyles,
-        },
-      ]}
-    />
+    <View style={styles.container}>
+      <Container>
+        <View style={styles.mainWrapper}>
+          <FlatList
+            data={data}
+            renderItem={_renderItem}
+            keyExtractor={(item, index) => `${index}`}
+            extraData={props}
+            horizontal
+            pagingEnabled={true}
+            bounces={false}
+          />
+        </View>
+      </Container>
+    </View>
   );
 };
 
