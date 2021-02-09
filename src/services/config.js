4d7320636b76dd2ee24687fcc033239d98dcd4e9
sangnh <sangnh@futurify.vn>
2021-02-09 21:18:38 +0700

    PS: config header api

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,6 @@ import apisauce from 'apisauce';
 
 import configEnv from 'config';
 import {TIME_OUT} from 'constants';
-
 var config = {
   url: configEnv.api_url,
   baseURL: configEnv.api_url,
@@ -13,6 +12,8 @@ export const api = apisauce.create({
   baseURL: config.baseURL,
   timeout: config.timeout,
 });
+
+api.setHeader('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ7XCJpZFwiOjIwLFwiZnVsbE5hbWVcIjpcIlN1cGVyIEFkbWluXCIsXCJ1c2VybmFtZVwiOlwic3VwZXJhZG1pbjFAZ21haWwuY29tXCIsXCJwaG9uZU51bWJlclwiOlwiMDkwMDAwMDAwM1wiLFwiZ2VuZGVyXCI6XCJNXCIsXCJyb2xlc1wiOltcIlNVUEVSX0FETUlOXCJdLFwiZmVhdHVyZXNcIjpbXX0iLCJzY29wZXMiOlsiUk9MRV9TVVBFUl9BRE1JTiJdLCJpc3MiOiJodHRwczovL3Byb3N0eWxlZS52bi8iLCJpYXQiOjE2MTI4Nzk3MjksImV4cCI6MTYxMjg5NDEyOX0.Pd4JpqS9u7b0VRsyaFC0u6WcsnsS-qBDX-PElCKcWnIwYoRykY2e9TRx20sy6qu8ElwdSpIXXIldJqWfJOXAXA')
 
 export async function _fetch(method, path, data) {
   return api[method](path, data).then((res) => {
