b7fc75f968f374771fe9ad5ce1da9a0f11891e3f
sangnh <sangnh@futurify.vn>
2021-02-16 18:03:41 +0700

    Store Screen

---
+++ src/screens/Stores/styles.js
@@ -0,0 +1,9 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+import {center,flexRow} from 'theme/style'
+
+export default EStyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '$white',
+  },
+});
