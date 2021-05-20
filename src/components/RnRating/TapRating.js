d9ab72a417bc0d245a1163e3511d6ed9a2737d83
Nguyễn Tuấn Vũ <vunt@solazu.net>
2021-05-20 21:25:30 +0700

    Fix

--- src/components/RnRating/TapRating.js
+++ src/components/RnRating/TapRating.js
@@ -4,19 +4,22 @@ import {StyleSheet, Text, View} from 'react-native';
 import Star from './components/Star';
 const TapRating = (props) => {
   const [position, setPosition] = useState(props.defaultRating);
+
   useEffect(() => {
     const {defaultRating} = props;
-    if (defaultRating === null || defaultRating === undefined) {
-      setPosition(3);
+    if (!defaultRating ) {
+      setPosition(1);
     } else {
       setPosition(defaultRating);
     }
   }, [props.defaultRating]);
+  
   const renderStars = (rating_array) => {
     return _.map(rating_array, (star) => {
       return star;
     });
   };
+  
   const starSelectedInPosition = (position) => {
     const {onFinishRating} = props;
     if (typeof onFinishRating === 'function') {
@@ -24,6 +27,7 @@ const TapRating = (props) => {
     }
     setPosition(position);
   };
+  
   const {count, reviews, showRating, reviewColor, reviewSize} = props;
   const rating_array = [];
   const starContainerStyle = [styles.starContainer];
