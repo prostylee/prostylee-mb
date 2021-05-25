22b4425849eaa9ac1f45ab629fffe35b74abb565
oliverson <oliverson1989@gmail.com>
2021-05-25 13:11:54 +0700

    MB-38: result product search

---
+++ src/screens/Search/SearchResult/ProductSearchResult/ResultProductSearchResult/item.js
@@ -0,0 +1,25 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
+import styles from './styles';
+import {Chip, Divider} from 'react-native-paper';
+const FeaturedCategoriesItem = ({item, index, navigation}) => {
+  return (
+    <View style={styles.wrapItems}>
+      <TouchableOpacity>
+        <View style={styles.item}>
+          <Text style={styles.title}>Áo khoác</Text>
+        </View>
+      </TouchableOpacity>
+      <View style={{paddingLeft: 16, paddingRight: 16}}>
+        <Divider />
+      </View>
+    </View>
+  );
+};
+
+FeaturedCategoriesItem.defaultProps = {};
+
+FeaturedCategoriesItem.propTypes = {};
+
+export default FeaturedCategoriesItem;
