2d4b88ea507e318d702e6d9169d0ec27db446967
Phan thành sơn <oliverson1989@gmail.com>
2021-06-10 16:19:16 +0700

    add chat graphql from github source

---
+++ src/screens/ChatBox/ChatOne2One.js
@@ -0,0 +1,270 @@
+import React from 'react';
+import {FlatList, View} from 'react-native';
+
+import {ButtonOutlined} from 'components';
+
+import {useDispatch} from 'react-redux';
+import {commonActions} from 'reducers';
+import {API, Auth, graphqlOperation} from 'aws-amplify';
+import {TextButton, TextInputBorderBottom} from 'components';
+import {createChat, deleteChat} from '../../graphql/mutations';
+import {getChat, listChats} from '../../graphql/queries';
+import {onCreateChat, onDeleteChat} from '../../graphql/subscriptions';
+import {formatTime} from '../../utils/datetime';
+import {Button, Divider, List, overlay, Text, useTheme} from 'react-native-paper';
+
+const DEFAULT_CHAT_GROUP_ID = 'USER_2_USER'; // Rule: USER_2_USER
+
+const ChatOne2One = (props) => {
+  const theme = useTheme();
+  const backgroundColor = overlay(2, theme.colors.surface);
+  const dispatch = useDispatch();
+  const [chat, setChat] = React.useState('');
+  const [parentChat, setParentChat] = React.useState('');
+  const [chats, setChats] = React.useState([]);
+  const [childrenChats, setChildrenChats] = React.useState([]);
+  const [currentUserName, setCurrentUserName] = React.useState('');
+
+  React.useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then((user) => {
+        console.log('USER ' + JSON.stringify(user, null, 4));
+        setCurrentUserName(user.username);
+      })
+      .catch((err) => console.log(err));
+    executeListChats();
+  }, [currentUserName]);
+
+  const executeListChats = async () => {
+    dispatch(commonActions.toggleLoading(true));
+    const user = await Auth.currentAuthenticatedUser();
+    API.graphql(
+      graphqlOperation(listChats, {
+        filter: {
+          parentId: {eq: DEFAULT_CHAT_GROUP_ID},
+          participantUserIds: {contains: user.attributes.sub},
+          // ownerFullname: {contains: 'Loc Nguyen'}, // TODO used for search
+        },
+        // limit: 4, // paging
+        // nextToken: null, // fill token to get data of next page
+      }),
+    )
+      .then((result) => {
+        console.log('Chats ' + JSON.stringify(result));
+        setChats(result.data.listChats.items);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    dispatch(commonActions.toggleLoading(false));
+  };
+
+  const executeGetChat = (chatId) => {
+    console.log('executeGetChat ' + chatId);
+    dispatch(commonActions.toggleLoading(true));
+    API.graphql(
+      graphqlOperation(getChat, {
+        id: chatId,
+      }),
+    )
+      .then((result) => {
+        console.log('Chat ' + JSON.stringify(result));
+        setChildrenChats(result.data.getChat.childrens.items);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    dispatch(commonActions.toggleLoading(false));
+  };
+
+  React.useEffect(() => {
+    const createChatListener = API.graphql(
+      graphqlOperation(onCreateChat),
+    ).subscribe({
+      next: (chatData) => {
+        const addedChat = chatData.value.data.onCreateChat;
+        console.log('addedChat ' + JSON.stringify(addedChat));
+
+        if (addedChat.parentId !== DEFAULT_CHAT_GROUP_ID) {
+          console.log('NEW child');
+          const updatedChats = [...childrenChats];
+          updatedChats.push(addedChat);
+          setChildrenChats(updatedChats);
+        } else {
+          console.log('NEW root');
+          const updatedChats = [...chats];
+          updatedChats.push(addedChat);
+          setChats(updatedChats);
+        }
+      },
+    });
+
+    const deleteChatListener = API.graphql(
+      graphqlOperation(onDeleteChat),
+    ).subscribe({
+      next: (chatData) => {
+        const deletedChat = chatData.value.data.onDeleteChat;
+        console.log('deletedChat ' + JSON.stringify(deletedChat));
+
+        if (deletedChat.parentId !== DEFAULT_CHAT_GROUP_ID) {
+          const updatedChats = childrenChats.filter(
+            (cmt) => cmt.id !== deletedChat.id,
+          );
+          setChildrenChats(updatedChats);
+        } else {
+          const updatedChats = chats.filter((cmt) => cmt.id !== deletedChat.id);
+          setChats(updatedChats);
+        }
+      },
+    });
+
+    dispatch(commonActions.toggleLoading(false));
+
+    return () => {
+      if (createChatListener) {
+        createChatListener.unsubscribe();
+      }
+      if (deleteChatListener) {
+        deleteChatListener.unsubscribe();
+      }
+    };
+  }, [chats, childrenChats, dispatch]);
+
+  const onChangeChat = (text) => {
+    setChat(text);
+  };
+
+  const addChatHandler = async () => {
+    console.log('addChatHandler ' + chat);
+    if (!chat) {
+      return;
+    }
+
+    const user = await Auth.currentAuthenticatedUser();
+    const response = await API.graphql(
+      graphqlOperation(createChat, {
+        input: {
+          parentId: parentChat ? parentChat.id : DEFAULT_CHAT_GROUP_ID,
+          ownerId: user.attributes.sub,
+          owner: user.username,
+          ownerFullname: 'Giang Phan',
+          participantUserIds: ['27c3092c-9f1b-49eb-ba68-ab3f091d1347', 'c2ec8d7f-6f7f-4c21-b02a-380d2eaaea1a'], // ['created-user-id', 'participant-user-id'], // TODO fill user id would like to chat
+          imageUrls: [
+            'https://pbs.twimg.com/profile_images/3390520999/c167795c6ab9ccad370ce53dbe85fd05.jpeg',
+            'https://s3.amazonaws.com/ionic-marketplace/m-fashion/icon.jpg',
+          ], // TODO fill image urls if user attached images in chat
+          content: chat,
+          createdAt: new Date().toISOString(), // "2021-02-18T15:41:16Z"
+        },
+      }),
+    );
+    console.log(
+      'Submit chat successfully with response' + JSON.stringify(response),
+    );
+    setChat('');
+  };
+
+  const replyChatHandler = (item) => {
+    console.log('replyChatHandler ' + JSON.stringify(item));
+    setParentChat(item);
+    executeGetChat(item.id);
+  };
+
+  const deleteChatHandler = async (item) => {
+    console.log('deleteChatHandler ' + item.id);
+    dispatch(commonActions.toggleLoading(true));
+    const res = await API.graphql(
+      graphqlOperation(deleteChat, {input: {id: item.id}}),
+    );
+    dispatch(commonActions.toggleLoading(false));
+    console.log('Delete successfully with response ' + JSON.stringify(res));
+  };
+
+  const _renderItem = ({item, index}) => {
+    return (
+      <List.Item
+        title={formatTime(item.createdAt)}
+        description={item.content + '-' + item.owner}
+        left={(props) => (
+          <List.Icon
+            {...props}
+            icon={
+              item.owner === currentUserName ? 'chevron-left' : 'chevron-right'
+            }
+            color={item.owner === currentUserName ? '#205da0' : '#01ab01'}
+          />
+        )}
+        right={(props) => (
+          <>
+            {item.owner === currentUserName && (
+              <Button icon="delete" onPress={() => deleteChatHandler(item)} />
+            )}
+            <Button icon="reply" onPress={() => replyChatHandler(item)} />
+          </>
+        )}
+      />
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <View style={{padding: 20}}>
+        <TextInputBorderBottom
+          hint={'Enter your chat here'}
+          value={chat}
+          onChangeText={(text) => onChangeChat(text)}
+          style={{width: '100%'}}
+          autoFocus={true}
+        />
+      </View>
+
+      <ButtonOutlined onPress={addChatHandler} label={'Add'} />
+
+      <View style={{height: '70%', width: '100%', padding: 20}}>
+        <View style={{flex: 1}}>
+          {chats && chats.length > 0 && (
+            <FlatList
+              contentContainerStyle={[
+                {backgroundColor: theme.colors.background},
+                {backgroundColor},
+              ]}
+              ItemSeparatorComponent={() => <Divider />}
+              data={chats}
+              renderItem={_renderItem}
+              keyExtractor={(item, index) => `${item.id}`}
+            />
+          )}
+        </View>
+
+        <View style={{flex: 1}}>
+          <View style={{paddingBottom: 20}} />
+          <TextButton
+            onPress={() => {
+              setParentChat(null);
+              setChildrenChats([]);
+            }}
+            label={'Back to parent'}
+          />
+
+          <Text>
+            {parentChat ? 'Parent: ' + parentChat.content : 'No parent'}
+          </Text>
+          {childrenChats && childrenChats.length > 0 && (
+            <FlatList
+              contentContainerStyle={[
+                {backgroundColor: theme.colors.background},
+                {backgroundColor},
+              ]}
+              ItemSeparatorComponent={() => <Divider />}
+              data={childrenChats}
+              renderItem={_renderItem}
+              keyExtractor={(item, index) => `${item.id}`}
+            />
+          )}
+        </View>
+      </View>
+    </React.Fragment>
+  );
+};
+
+export default ChatOne2One;
--- src/screens/ChatBox/index.js
+++ src/screens/ChatBox/index.js
@@ -1,47 +1,21 @@
 import React from 'react';
-import {View, Text, Image, TextInput} from 'react-native';
-import styles from './styles';
-import {Header, ThemeView} from 'components';
-import Icon from 'react-native-vector-icons/Ionicons';
-import ProductItem from './ProductItem';
-import FooterItem from './FooterItem';
-import Chat from './Chat';
-const ChatBox = ({navigation}) => {
+import {Header, ThemeView} from '../../components';
+import Comment from './Comment';
+import ChatOne2One from './ChatOne2One';
+
+const Chat = (props) => {
+
   return (
-    <ThemeView style={{flex: 1}} isFullView>
-      <Header
-        containerStyle={styles.headerContain}
-        middleComponent={
-          <View style={styles.leftHeader}>
-            <Icon name="chevron-back-outline" size={20} color="white" />
-            <View style={styles.title}>
-              <Image
-                style={styles.avatar}
-                source={require('../../assets/images/avatar.jpg')}
-              />
-              <Text style={styles.name}>Minh Thi</Text>
-            </View>
-          </View>
-        }
-        rightComponent={
-          <View style={styles.headerRight}>
-            <Icon name="call-outline" size={22} color="white" />
-            <Icon
-              style={styles.headerRightIcon}
-              name="ellipsis-horizontal"
-              size={22}
-              color="white"
-            />
-          </View>
-        }
-      />
-      <ProductItem />
-      <View style={{flex: 1}}>
-        <Chat />
-      </View>
-      <FooterItem />
+    <ThemeView isFullView>
+      <Header isDefault title={'Chat'} />
+      {/*<Comment />*/}
+      <ChatOne2One />
     </ThemeView>
   );
 };
 
-export default ChatBox;
+Chat.defaultProps = {};
+
+Chat.propTypes = {};
+
+export default Chat;
