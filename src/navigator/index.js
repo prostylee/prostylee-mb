71444be89bdad38e90fc8a0a138b92c0324b6b22
Phan thành sơn <oliverson1989@gmail.com>
2021-06-11 23:26:03 +0700

    fix navigation

--- src/navigator/index.js
+++ src/navigator/index.js
@@ -96,7 +96,7 @@ function SignedIn() {
         gestureDirection: 'horizontal',
         cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
       }}
-      initialRouteName="Stores"
+      initialRouteName="Home"
       mode="card"
       headerMode="none"
       animation="fade">
