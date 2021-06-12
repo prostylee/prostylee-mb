564d298bceb310e0675e955e057291d6e45860f9
Lê Quốc Nhâm <lqnham@gmail.com>
2021-06-12 21:48:20 +0700

    config push notification

--- android/app/src/main/java/com/prostylee/MainActivity.java
+++ android/app/src/main/java/com/prostylee/MainActivity.java
@@ -1,9 +1,9 @@
 package com.prostylee;
 
 import com.facebook.react.ReactActivity;
+import com.prostylee.PushNotificationManager;
 
 public class MainActivity extends ReactActivity {
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -12,4 +12,14 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "prostylee";
   }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize PinpointManager
+        PushNotificationManager manager = new PushNotificationManager();
+        manager.getPinpointManager(getApplicationContext());
+    }
 }
---
+++ android/app/src/main/java/com/prostylee/PushNotificationManager.java
@@ -0,0 +1,63 @@
+package com.prostylee;
+
+import android.app.Application;
+import android.content.Context;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.amazonaws.RNAWSCognitoPackage;
+import com.amazonaws.RNAWSCognitoPackage;
+import com.amazonaws.RNAWSCognitoPackage;
+import com.amazonaws.RNAWSCognitoPackage;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+// 1. Import the plugin class.
+import com.microsoft.codepush.react.CodePush;
+
+public class PushNotificationManager extends AppCompatActivity{
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private static PinpointManager pinpointManager;
+
+    public static PinpointManager getPinpointManager(final Context applicationContext) {
+        if (pinpointManager == null) {
+            final AWSConfiguration awsConfig = new AWSConfiguration(applicationContext);
+            AWSMobileClient.getInstance().initialize(applicationContext, awsConfig, new Callback<UserStateDetails>() {
+                @Override
+                public void onResult(UserStateDetails userStateDetails) {
+                    Log.i("INIT", userStateDetails.getUserState());
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    Log.e("INIT", "Initialization error.", e);
+                }
+            });
+
+            PinpointConfiguration pinpointConfig = new PinpointConfiguration(
+                    applicationContext,
+                    AWSMobileClient.getInstance(),
+                    awsConfig);
+
+            pinpointManager = new PinpointManager(pinpointConfig);
+
+            FirebaseInstanceId.getInstance().getInstanceId()
+                    .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                            if (!task.isSuccessful()) {
+                                Log.w(TAG, "getInstanceId failed", task.getException());
+                                return;
+                            }
+                            final String token = task.getResult().getToken();
+                            Log.d(TAG, "Registering push notifications token: " + token);
+                            pinpointManager.getNotificationClient().registerDeviceToken(token);
+                        }
+                    });
+        }
+        return pinpointManager;
+    }
+}
