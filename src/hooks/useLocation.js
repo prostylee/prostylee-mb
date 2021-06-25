7e028485542a19d1a9ec2f8ac884e65157596cd9
etkgjt <canlong102@gmail.com>
2021-06-25 13:47:27 +0700

    update location

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react';
-import Geolocation from '@react-native-community/geolocation';
-import {Platform, PermissionsAndroid} from 'react-native';
+
 import Geocoder from 'react-native-geocoding';
+import RNLocation from 'react-native-location';
 
 const useLocation = () => {
   const [location, setLocation] = useState({
@@ -9,59 +9,63 @@ const useLocation = () => {
     lon: 0,
     address: '',
   });
-  const requestLocationPermission = async () => {
-    Geocoder.init('AIzaSyDIqkfXbjBkSQeAaFZchfHB9k6CzF6ctsI');
-    if (Platform.OS === 'ios') {
-      getOneTimeLocation();
-    } else {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-          {
-            title: 'Location Access Required',
-            message: 'This App needs to Access your location',
-          },
-        );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          getOneTimeLocation();
-        } else {
-          console.log('ACCESS LOCATION DENIED');
-        }
-      } catch (err) {
-        console.warn(err);
-      }
-    }
-  };
 
-  const getOneTimeLocation = () => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        const currentLongitude = JSON.stringify(position.coords.longitude);
-        const currentLatitude = JSON.stringify(position.coords.latitude);
-        setLocation({...location, lon: currentLongitude, lat: currentLatitude});
-      },
-      (error) => {
-        console.log('GET LOCATION ERROR', error);
-      },
-      {
-        enableHighAccuracy: false,
-        timeout: 30000,
-        maximumAge: 1000,
-      },
-    );
-  };
   useEffect(() => {
-    if (location.lat && location.lon)
+    Geocoder.init('AIzaSyDIqkfXbjBkSQeAaFZchfHB9k6CzF6ctsI');
+    let locationSubscription = null;
+    (async () => {
+      RNLocation.configure({
+        distanceFilter: 100, // Meters
+        desiredAccuracy: {
+          ios: 'best',
+          android: 'highAccuracy',
+        },
+      });
+      RNLocation.requestPermission({
+        ios: 'whenInUse',
+        android: {
+          detail: 'fine',
+          rationale: {
+            title: 'Location permission',
+            message: 'We use your location to demo the library',
+            buttonPositive: 'OK',
+            buttonNegative: 'Cancel',
+          },
+        },
+      })
+        .then((granted) => {
+          if (granted) {
+            locationSubscription = RNLocation.subscribeToLocationUpdates(
+              (locations) => {
+                setLocation({
+                  lat: locations[0].latitude,
+                  lon: locations[0].longitude,
+                });
+              },
+            );
+          }
+        })
+        .catch((err) => {
+          console.log('GRANTED PERMISSION ERR', err);
+        });
+    })();
+
+    return () => {
+      locationSubscription && locationSubscription();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (location.lat && location.lon) {
       Geocoder.from(location.lat, location.lon)
         .then((json) => {
-          var addressComponent = json.results[0].address_components[0];
-          console.log('YOUR ADDRESS', addressComponent);
+          const addressComponent = json.results[0].address_components[0];
+          console.log('GET ADDRESS SUCCESS', addressComponent);
         })
         .catch((error) => console.log('GET ADDRESS ERROR', error));
-  }, [location.lat, location.lon]);
-  useEffect(() => {
-    requestLocationPermission();
-  }, []);
+    }
+  }, [location]);
+
   return location;
 };
 
