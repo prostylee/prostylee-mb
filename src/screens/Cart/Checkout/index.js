ff1878eab1da6d6d59ae4bfbb36ef8f0aeba9b33
Loc Nguyen Thien <locnguyen3039@gmail.com>
2021-07-02 15:00:59 +0700

    update cart

--- src/screens/Cart/Checkout/index.js
+++ src/screens/Cart/Checkout/index.js
@@ -3,19 +3,48 @@ import React from 'react';
 import {View} from 'react-native';
 import i18n from 'i18n';
 import isEmpty from 'lodash/isEmpty';
-import {useSelector} from 'react-redux';
-import {getSelectedCartAddressSelector} from 'redux/selectors/cart';
+import {useSelector, useDispatch} from 'react-redux';
+import {
+  getSelectedCartAddressSelector,
+  getListCartAddressSelector,
+} from 'redux/selectors/cart';
 import {ThemeView, Header} from 'components';
 import ListProduct from './ListProduct';
+import {cartActions} from 'reducers';
 
 const Cart = ({navigation}) => {
+  const dispatch = useDispatch();
   const selectedCartAddress = useSelector((state) =>
     getSelectedCartAddressSelector(state),
+  );
+
+  const listCartAddress = useSelector((state) =>
+    getListCartAddressSelector(state),
   );
 
   const validateButton = React.useCallback(() => {
     return isEmpty(selectedCartAddress);
   }, [selectedCartAddress]);
+
+  React.useEffect(() => {
+    dispatch(cartActions.getListCartAddress());
+  }, []);
+
+  console.log(listCartAddress);
+
+  React.useEffect(() => {
+    if (listCartAddress && listCartAddress.length) {
+      if (isEmpty(selectedCartAddress)) {
+        const defaultAddress =
+          listCartAddress.find((item) => item.priority) || null;
+        dispatch(
+          cartActions.setSelectedCartAddress(
+            defaultAddress || listCartAddress[0],
+          ),
+        );
+      }
+    }
+  }, [selectedCartAddress, listCartAddress]);
 
   return (
     <ThemeView style={styles.container} isFullView>
