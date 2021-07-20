5c2c515819aa28cee7272830004a6447fcdfbd4c
etkgjt <canlong102@gmail.com>
2021-07-20 13:10:02 +0700

    update push noti

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import {PersistGate} from 'redux-persist/integration/react';
 
 import Index from './src';
 
+import messaging from '@react-native-firebase/messaging';
+
 import {Provider as PaperProvider, DefaultTheme} from 'react-native-paper';
 const theme = {
   ...DefaultTheme,
@@ -13,6 +15,22 @@ const theme = {
 const App = () => {
   useEffect(() => {
     disableLogBox();
+    (async function requestUserPermission() {
+      console.log('REQUEST PER');
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+      }
+    })();
+
+    messaging()
+      .getToken()
+      .then((res) => console.log('FCM TOKEN', res))
+      .catch((error) => console.log('GET FCM TOKEN ERORR', er));
   }, []);
   return (
     <Provider store={store}>
